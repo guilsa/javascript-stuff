a319f0c2290e8075fe31a91c6be71d97ee3f1d5c
Guilherme Sa <guilsa001@gmail.com>
2016-03-13 19:45:28 -0700

    Add nested repo nav now persists

--- react-router-tutorial/modules/Repos.js
+++ react-router-tutorial/modules/Repos.js
@@ -1,19 +1,17 @@
 import React from 'react'
-// Repos.js
 import { Link } from 'react-router'
-// ...
+import NavLink from './NavLink'
+
 export default React.createClass({
   render() {
     return (
       <div>
         <h2>Repos</h2>
-
-        {/* add some links */}
         <ul>
-          <li><Link to="/repos/react/react-router">React Router</Link></li>
+          <li><NavLink to="/repos/react/react-router">React Router</NavLink></li>
           <li><Link to="/repos/facebook/react">React</Link></li>
         </ul>
-
+        {this.props.children}
       </div>
     )
   }
