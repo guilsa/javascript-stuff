6c284f9286b54420102c72287418d25d6907d663
Guilherme Sa <guilsa001@gmail.com>
2016-03-21 00:10:10 -0700

    Finish adding lifecycle events

--- react-github-battle/app/components/ConfirmBattle.js
+++ react-github-battle/app/components/ConfirmBattle.js
@@ -1,9 +1,9 @@
 var React = require('react');
 
 var ConfirmBattle = function(props){
-  return (
-    <div>Confirm Battle</div>
-  );
+  return props.isLoading === true
+    ? <p> LOADING </p>
+    : <p> CONFIRM BATTLE </p>
 }
 
 module.exports = ConfirmBattle;
--- react-github-battle/app/containers/ConfirmBattleContainer.js
+++ react-github-battle/app/containers/ConfirmBattleContainer.js
@@ -6,14 +6,31 @@ var ConfirmBattleContainer = React.createClass({
      router: React.PropTypes.object.isRequired
   },
   getInitialState: function () {
+    console.log('getInitialState');
     return {
       isLoading: true,
       playerInfo: []
     }
   },
+  componentWillMount: function () {
+    console.log('componentWillMount');
+  },
+  componentDidMount: function () {
+    var query = this.props.location.query;
+    console.log('componentDidMount');
+    // fetch info from github then update state
+  },
+  componentWillReceiveProps: function () {
+    console.log('componentWillReceiveProps');
+  },
+  componentWillUnmount: function () {
+    console.log('componentWillUnmount');
+  },
   render: function(){
     return (
-      <ConfirmBattle />
+      <ConfirmBattle
+        isLoading={this.state.isLoading}
+        playersInfo={this.state.playersInfo} />
     );
   }
 });
