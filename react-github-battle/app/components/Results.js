3a44945efe6e3beba1b0a30539eea9f180b2d724
Guilherme Sa <guilsa001@gmail.com>
2016-03-21 22:52:25 -0700

    Complete Results component

--- react-github-battle/app/components/Results.js
+++ react-github-battle/app/components/Results.js
@@ -1,13 +1,50 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
+var Link = require('react-router').Link;
 
-function puke (obj) {
-  return <pre>{ JSON.stringify(obj, 2, ' ')}</pre>
+function StartOver (){
+  return (
+    <div className="col-sm-12" styles={styles.space}>
+      <Link to="/playerOne">
+        <button type="button" className="btn btn-lg btn-danger">Start Over</button>
+      </Link>
+    </div>
+  )
 }
 
 function Results (props) {
+  if (props.isLoading === true) {
+    return (
+      <p> LOADING </p>
+    )
+  }
+
+  if (props.scores[0] === props.scores[1]){
+    return (
+      <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>It's a tie!</h1>
+        <StartOver />
+      </div>
+    )
+  }
+  var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+  var loosingIndex = winningIndex === 0 ? 1 : 0;
   return (
-    <div> Results {puke(props)} </div>
+    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+      <h1>Results</h1>
+      <div className="col-sm-8 col-sm-offset-2">
+        <UserDetailsWrapper header="Winner">
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]}/>
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header="Looser">
+          <UserDetails score={props.scores[loosingIndex]} info={props.playersInfo[loosingIndex]}/>
+        </UserDetailsWrapper>
+      </div>
+      <StartOver />
+    </div>
   )
 }
 
